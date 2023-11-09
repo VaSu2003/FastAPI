919d81bb1d5e432e61434e9f63c13462a7cb8bca
Vaibhav <vaibhav.shukla98871174@gmail.com>
2023-11-09 22:25:58 +0530

    saving posts in an array and fetching it from array mem using get posts

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [{'title': 'title of post 1', 'content': 'content of post 1', 'id': 1}, {'title':'favourite food', 'content':'I like cheese'}]
+
 
 @app.get("/")
 async def root():
@@ -17,10 +19,10 @@ async def root():
 
 @app.post("/posts")
 def get_posts():
-    return {"data" : "this is your posts"}
+    return {"data" : my_posts}
 
 @app.post("/createposts")
 def create_posts(new_post:Post):
     print(new_post)
-    print(new_post.model_dump)
+    print(new_post.model_dump())
     return {"data": "new_post"}
